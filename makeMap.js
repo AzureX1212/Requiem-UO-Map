587c19e08a224d5b283d2bd14d7c108da931c88f
AzureX1212 <tony.e.avila@gmail.com>
2017-09-27 01:17:05 -0500

    Moved map creation outside of script to solve an error on some mobile browsers.

--- makeMap.js
+++ makeMap.js
@@ -1,11 +1,13 @@
 var map = L.map('map', {
 crs: L.CRS.Simple,
-minZoom: -3,
+minZoom: -2,
 maxZoom: 1,
-zoomDelta: 0.25
+zoomDelta: 0.25,
+layers: [poi]
 });
 var bounds = [[0,0], [4096,4776]];
 map.fitBounds(bounds);
+map.setMaxBounds(bounds);
 var image = L.imageOverlay('ReqMap.png', bounds).addTo(map);
 map.attributionControl.addAttribution('The First Coast v1 Created by Azure for <a target="_blank" href="http://www.13thrones.com/wordpress/", class="your_class">Requiem</a>');
 L.Control.Watermark = L.Control.extend({
